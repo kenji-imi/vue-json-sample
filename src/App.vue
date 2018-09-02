4c0e0d355a35fa28ea9ae79b1fdc8f9b4f0dc244
kenji imai <kenji.imai@gmail.com>
2018-09-02 13:33:05 +0900

    Add json app files

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2>東京23区一覧</h2>
+    <div v-for="(city, i) in cities" v-bind:key="i">
+      {{city.id}} : {{city.name}}<br>
+    </div>
+  </div>
+</template>
+
+<script>
+import cities from "./assets/cities.json"
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      cities
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
